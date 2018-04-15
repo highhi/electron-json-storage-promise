2dab717908b814861c5bc770992b6b9ce7fe4d3d
kazu <kazu8160@gmail.com>
2018-04-16 00:08:11 +0900

    first commit

---
+++ test/electron-json-storage-promise.test.js
@@ -0,0 +1,67 @@
+'use strict';
+const fs = require('fs-extra');
+const path = require('path');
+const storage = require('../lib/electron-json-storage-promise');
+
+const TEMP_FILE = 'foo.json';
+const TEMP_DIR_PATH = path.join(__dirname, '../tmp');
+const TEMP_FILE_PATH = path.join(TEMP_DIR_PATH, TEMP_FILE);
+
+storage.setStoragePath(TEMP_DIR_PATH);
+
+describe('electron-json-storage-promise', () => {
+  beforeEach(() => {    
+    return storage.set(TEMP_FILE, { foo: 'foo' });
+  });
+
+  afterEach(() => {
+    return storage.clear();
+  });
+
+  describe('.set', () => {
+    it('should be able to store a valid JSON object', async () => {
+      const data = await storage.set('bar', { bar: 'bar' });
+      const exists = await fs.pathExists(TEMP_DIR_PATH + '/bar.json');
+      expect(data).toEqual({ bar: 'bar' });
+      expect(exists).toBe(true);
+    });
+
+    it('should throw erro if invalid second argument', () => {
+      expect(() => {
+        storage.set(TEMP_FILE, 'baz');
+      }).toThrow('The second argument must be a plain object.');
+    });
+  });
+
+  describe('.get', () => {
+    it('should return storage object if key exist', () => {
+      storage.get('foo').then(data => {
+        expect(data).toEqual({ foo: 'foo' });
+      });
+    });
+
+    it('should return undefined if key not exist', () => {
+      storage.get('bar').then(data => {
+        expect(data).toBeUndefined();
+      });
+    });
+  });
+
+  describe('.has', () => {
+    it('should return true if key exist', () => {
+      expect(storage.has('foo')).toBe(true);
+    })
+
+    it('should return false if key not exist', () => {
+      expect(storage.has('bar')).toBe(false);
+    });
+  });
+
+  describe('.keys', () => {
+    it('should return array contains all keys', async () => {
+      await storage.set('bar', { bar: 'bar' })
+      const keys = await storage.keys();
+      expect(keys).toEqual(expect.arrayContaining(['foo', 'bar']));
+    });
+  });
+});
