d272a8b428f675290e2da3da5586232525120755
kazu <kazu8160@gmail.com>
2018-04-22 22:45:03 +0900

    remove lib dir

--- lib/electron-json-storage-promise.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function getStoragePath(): string;
-export declare function setStoragePath(dir: string): void;
-export declare function get(key: string): Promise<object>;
-export declare function set(key: string, data: object): Promise<object>;
-export declare function has(key: string): boolean;
-export declare function keys(): Promise<string[]>;
-export declare function remove(key: string): Promise<void>;
-export declare function clear(): Promise<void[]>;
